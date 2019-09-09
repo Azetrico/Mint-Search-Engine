65fb1407e7d43763eef4c874f758ba8eb70e7091
William Lassalle <william@azetrico.tk>
2019-09-09 16:28:50 -0400

    card styling updated

--- search.js
+++ search.js
@@ -31,7 +31,7 @@ function getSearchData(data) {
     var htmlse = ""
     data['items'].forEach(function(element){
       if (element['kind'] == "customsearch#result") {
-        htmlse += '<div class="card w-75"><div class="card-body"><h5 class="card-title">' + element['title'] + '</h5><a href="' + element['link'] + '">' + element['displayLink'] + '</a><p class="card-text">' + element['snippet'] + '</p></div> </div>'
+        htmlse += '<div class="card w-100"><div class="card-body"><h5 class="card-title">' + element['title'] + '</h5><a href="' + element['link'] + '">' + element['displayLink'] + '</a><p class="card-text">' + element['snippet'] + '</p></div> </div>'
       }
     });
     document.getElementById("content").innerHTML = htmlse
