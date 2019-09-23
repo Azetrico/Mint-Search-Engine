eb81ae68f4a64ce0b28cca6a9192a92791d247f2
William L <william.justus1117@gmail.com>
2019-09-23 18:29:31 -0400

    Update imageSearch.js

--- imageSearch.js
+++ imageSearch.js
@@ -36,7 +36,7 @@ function getSearchData(data) {
 	}
     data['items'].forEach(function(element){
       if (element['kind'] == "customsearch#result") {
-        htmlse += '<div class="card w-100"><div class="card-body"><img style="max-width: 400; max-height: 400;" src="' + element['link'] + '</img></div></div>'
+        htmlse += '<div class="card w-100"><div class="card-body"><img style="max-width: 400; max-height: 400;" src="' + element['link'] + '"></img></div></div>'
       }
     });
     document.getElementById("content").innerHTML = htmlse
